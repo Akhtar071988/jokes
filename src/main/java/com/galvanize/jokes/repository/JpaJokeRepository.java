f3faa61060d3c638a66764c1c13d53ab8f77dc44
Sami Akhtar <44879596+Akhtar071988@users.noreply.github.com>
2020-04-01 17:07:01 -0400

    all tests pass

--- src/main/java/com/galvanize/jokes/repository/JpaJokeRepository.java
+++ src/main/java/com/galvanize/jokes/repository/JpaJokeRepository.java
@@ -3,6 +3,7 @@ package com.galvanize.jokes.repository;
 import com.galvanize.jokes.entity.Category;
 import com.galvanize.jokes.entity.Joke;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
--- src/test/java/com/galvanize/jokes/service/JpaJokeServiceTest.java
+++ src/test/java/com/galvanize/jokes/service/JpaJokeServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
